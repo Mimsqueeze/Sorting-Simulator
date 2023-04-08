be57cee8a007ebec48b400aa7a8d4c9f6ec1b925
Minsi Hu <101283845+Mimsqueeze@users.noreply.github.com>
2023-04-08 14:27:20 -0400

    Added mains

--- MainA.java
+++ MainA.java
@@ -1,4 +1,4 @@
-public class Main {
+public class MainA {
     public static void main(String[] args) {
         System.out.println("Hello world");
     }
--- MainM.java
+++ MainM.java
@@ -1,4 +1,4 @@
-public class Main {
+public class MainM {
     public static void main(String[] args) {
         System.out.println("Hello world");
     }
