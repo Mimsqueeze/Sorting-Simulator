f0911cf663687931b1c5ce66510d79ececa204be
allyycat <74456093+allyycat@users.noreply.github.com>
2023-04-08 16:31:51 -0400

    Update MainA.java

--- src/MainA.java
+++ src/MainA.java
@@ -10,16 +10,28 @@ public class MainA {
             }
         });
     }
-
+    String[] sortingAlgs = {"Bubble Sort", "Selection Sort", "Insertion Sort", "Quick Sort", "Merge Sort", "Heap Sort"};
     JFrame frame;
     myPanel panel;
+    JComboBox<String> dropDown;
+    JButton run = new JButton("Simulate");
     MainA() {
         frame = new JFrame("Sorting Simulator");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         panel = new myPanel();
-        panel.setPreferredSize(new Dimension(600, 600));
-        frame.pack();
+        panel.setLayout(new GridLayout(20, 2));
+        panel.setPreferredSize(new Dimension(1000, 600));
+        dropDown = new JComboBox<>(sortingAlgs);
         frame.setContentPane(panel);
+        panel.add(new JLabel("Sorting Algorithm Simulator"));
+        panel.add(Box.createRigidArea(new Dimension(10, 0)));
+        panel.add(new JLabel("Sorting Algorithm"));
+        panel.add(dropDown);
+        panel.add(Box.createRigidArea(new Dimension(10, 0)));
+        panel.add(new JLabel("Enter Size"));
+        panel.add(new JTextField());
+        panel.add(Box.createRigidArea(new Dimension(10, 0)));
+        panel.add(run);
         frame.pack();
         frame.setVisible(true);
         panel.paintComponent(panel.getGraphics());
@@ -32,8 +44,9 @@ public class MainA {
      class myPanel extends JPanel {
          public void paintComponent(Graphics g) {
              super.paintComponent(g);
-             g.setColor(Color.RED);
-             g.fillRect(0,0,500,500);
+             //g.setColor(Color.RED);
+             //g.fillRect(0,0,500,500);
          }
      }
+
 }
