46a082c8bd55a72ecc7ddb9d2d7df249a8aa19eb
allyycat <74456093+allyycat@users.noreply.github.com>
2023-04-08 15:04:54 -0400

    Update MainA.java

--- MainA.java
+++ MainA.java
@@ -1,5 +1,39 @@
+import java.awt.Dimension;
+
+import javax.swing.*;
+
 public class MainA {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                runGUI();
+            }
+        });
     }
+
+    JFrame frame;
+    myPanel panel;
+    MainA() {
+        frame = new JFrame("Sorting Simulator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        panel = new myPanel();
+        panel.setPreferredSize(new Dimension(600, 600));
+        frame.pack();
+        frame.setContentPane(panel);
+        frame.pack();
+        frame.setVisible(true);
+        panel.paintComponent(panel.getGraphics());
+    }
+    private static void runGUI() {
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        MainA drive = new MainA();
+
+    }
+     class myPanel extends JPanel {
+         public void paintComponent(Graphics g) {
+             super.paintComponent(g);
+             g.setColor(Color.RED);
+             g.fillRect(0,0,500,500);
+         }
+     }
 }
