ec870db45dda0284ee047f7f9b0ba1af491902ba
Minsi Hu <101283845+Mimsqueeze@users.noreply.github.com>
2023-04-14 00:36:07 -0400

    Revert "Trying to speed up running"

--- src/graphScreen.java
+++ src/graphScreen.java
@@ -1,29 +1,26 @@
 import java.awt.*;
 import java.awt.geom.Rectangle2D;
 
-public class GraphScreen {
+public class graphScreen {
 
     // Reference to Main Program
     Main main;
 
-    // Reference to Graphics2D used by Main
-    Graphics2D graphics;
-
     // Constants based on desired window size
-    public static final int WIDTH= 1240; 
-    public static final int HEIGHT= 620;
-    public static final int HEADER= 75;
-    public static final int BARHEIGHT= HEIGHT - HEADER;
+    public static final int WIDTH = 1240; 
+    public static final int HEIGHT = 620;
+    public static final int HEADER = 75;
+    public static final int BARHEIGHT = HEIGHT - HEADER;
     
     // Constants defining the color of the graph
-    private static final Color BARCOLOR= Color.GREEN;
-    private static final Color SWAPCOLOR= Color.RED;
-    private static final Color COMPARECOLOR= Color.YELLOW;
-    private static final Color EMPTYCOLOR= Color.BLACK;
+    private static final Color BARCOLOR = Color.GREEN;
+    private static final Color SWAPCOLOR = Color.YELLOW;
+    private static final Color INSPECTCOLOR = Color.RED;
+    private static final Color EMPTYCOLOR = Color.BLACK;
 
     // Constants defining the color and size of the UI
-    private static final Color HEADERCOLOR= Color.BLACK;
-    private static final Color TEXTCOLOR= Color.WHITE;
+    private static final Color HEADERCOLOR = Color.BLACK;
+    private static final Color TEXTCOLOR = Color.WHITE;
 
     // Constant defining creation and dimensions of the restart button
     private static Rectangle2D restart;
@@ -35,100 +32,101 @@ public class GraphScreen {
     private int SIZE;
 
     // The array of positions to be highlighted
-    private int[] pointers= new int[0];
+    private int[] pointers = new int[0];
 
     // Statistics to be displayed
-    private long[] data;
+    private int numComparisons, numSwaps, numInsertions, numSims = 1;
+    private long totalTime= 0;
 
     // Boolean flags used in processing
     
-    private Constants.Mode mode;
+    private State state = State.NEITHER;
+    private boolean finished = false;
     
     // graphScreen constructor to create a new frame to render
-    GraphScreen(Main main, int size) {
-        this.main= main;
-        this.SIZE= size;
+    graphScreen(Main main) {
+        this.main = main;
     }
 
     // Function called to render each frame of the graph
-    public void render() {
-        this.graphics= (Graphics2D) main.panel.getGraphics();
+    public void render(Graphics2D g) {
 
         // Fill in the rectangles (bars) for the graph
-        for (int i= 0; i < SIZE; i++) {
+        for (int i = 0; i < SIZE; i++) {
 
             // Fill the bar
-            graphics.setColor(BARCOLOR);
-            graphics.fillRect(i*WIDTH/SIZE, HEIGHT - (BARHEIGHT*array[i]/SIZE), (WIDTH/SIZE)+1, HEADER + (BARHEIGHT*array[i]/SIZE));
+            g.setColor(BARCOLOR);
+            g.fillRect(i*WIDTH/SIZE, HEIGHT - (BARHEIGHT*array[i]/SIZE), (WIDTH/SIZE)+1, HEADER + (BARHEIGHT*array[i]/SIZE));
            
             // Fill space above the bar
-            graphics.setColor(EMPTYCOLOR);
-            graphics.fillRect(i*WIDTH/SIZE, HEADER, (WIDTH/SIZE)+1, BARHEIGHT - (BARHEIGHT*array[i]/SIZE));
+            g.setColor(EMPTYCOLOR);
+            g.fillRect(i*WIDTH/SIZE, HEADER, (WIDTH/SIZE)+1, BARHEIGHT - (BARHEIGHT*array[i]/SIZE));
         }
-        if (mode != Constants.Mode.DEFAULT && mode != Constants.Mode.FINISH) {
-            if (mode == Constants.Mode.COMPARE) { // comparing
-                graphics.setColor(COMPARECOLOR);
-            } else if (mode == Constants.Mode.SWAP) { // swapping
-                graphics.setColor(SWAPCOLOR);
+        if (state != State.NEITHER) {
+            if (state == State.INSPECTING) { // inspecting
+                g.setColor(INSPECTCOLOR);
+            } else if (state == State.SWAPPING) { // swapping
+                g.setColor(SWAPCOLOR);
+                
             }
+            state = State.NEITHER;
             // if sound enabled, split execution to play a sound while updating the highlighted positions
             if (main.soundOn) {
-                for (int i= 0; i < pointers.length; i++) {
-                    graphics.fillRect(pointers[i]*WIDTH/SIZE, HEIGHT - (BARHEIGHT*array[pointers[i]]/SIZE), (WIDTH/SIZE)+1, HEADER + (BARHEIGHT*array[pointers[i]]/SIZE));
+                for (int i = 0; i < pointers.length; i++) {
+                    g.fillRect(pointers[i]*WIDTH/SIZE, HEIGHT - (BARHEIGHT*array[pointers[i]]/SIZE), (WIDTH/SIZE)+1, HEADER + (BARHEIGHT*array[pointers[i]]/SIZE));
                 
                     Sound.makeSound(pointers[i], SIZE);
                 }
             } else {
-                for (int i= 0; i < pointers.length; i++)
-                    graphics.fillRect(pointers[i]*WIDTH/SIZE, HEIGHT - (BARHEIGHT*array[pointers[i]]/SIZE), (WIDTH/SIZE)+1, HEADER + (BARHEIGHT*array[pointers[i]]/SIZE));
+                for (int i = 0; i < pointers.length; i++)
+                    g.fillRect(pointers[i]*WIDTH/SIZE, HEIGHT - (BARHEIGHT*array[pointers[i]]/SIZE), (WIDTH/SIZE)+1, HEADER + (BARHEIGHT*array[pointers[i]]/SIZE));
             }
         }
-        // Fill Header Information
-        displayInformation();
+        // Fill Header
+        displayInformation(g);
     }
 
-    private void displayInformation() {
+    private void displayInformation(Graphics2D g) {
+
         // Fill header
-        graphics.setColor(HEADERCOLOR);
-        graphics.fillRect(0, 0, WIDTH, HEADER);
+        g.setColor(HEADERCOLOR);
+        g.fillRect(0, 0, WIDTH, HEADER);
 
         // Display title
-        graphics.setColor(TEXTCOLOR);
-        graphics.setFont(main.titleFont);
-        graphics.drawString((String) main.dropDown.getSelectedItem() + " (Run " + data[Constants.DATA_INDICES.NUM_SIMULATIONS] + ")", 0, 25);
+        g.setColor(TEXTCOLOR);
+        g.setFont(main.titleFont);
+        g.drawString((String) main.dropDown.getSelectedItem() + " (Run " + numSims + ")", 0, 25);
 
         // Display information
-        graphics.setFont(main.myFont);
+        g.setFont(main.myFont);
 
         // Splits execution based on finish status
-        if (mode != Constants.Mode.FINISH) {
-            // Unfinished, so display current information
-            graphics.drawString(
-                "Comparisons: " + String.format("%.2f", (double) data[Constants.DATA_INDICES.NUM_COMPARISONS]) + " " + 
-                "Swaps: "       + String.format("%.2f", (double) data[Constants.DATA_INDICES.NUM_SWAPS]) + " " +  
-                "Insertions: "  + String.format("%.2f", (double) data[Constants.DATA_INDICES.NUM_INSERTIONS]), 0, 50);
+        if (!finished) {
+            g.drawString(
+                "Comparisons: " + String.format("%.2f", (double) numComparisons) + " " + 
+                "Swaps: "       + String.format("%.2f", (double) numSwaps) + " " +  
+                "Insertions: "  + String.format("%.2f", (double) numInsertions), 0, 50);
 
-            if (data[Constants.DATA_INDICES.NUM_TIME] > 0)
-                graphics.drawString("Time (nanoseconds): " + String.format("%,d", data[Constants.DATA_INDICES.NUM_TIME]/(data[Constants.DATA_INDICES.NUM_SIMULATIONS]-1)), 0, 125);
+            if (totalTime > 0)
+                g.drawString("Time (nanoseconds): " + String.format("%,d", totalTime/(numSims-1)), 0, 125);
 
         } else {
-            // Finished, so display information and render the restart button
-            graphics.drawString(
-                "Avg. Comparisons: " + String.format("%.2f", (double) data[Constants.DATA_INDICES.NUM_COMPARISONS]) + " " + 
-                "Avg. Swaps: "       + String.format("%.2f", (double) data[Constants.DATA_INDICES.NUM_SWAPS]) + " " +  
-                "Avg. Insertions: "  + String.format("%.2f", (double) data[Constants.DATA_INDICES.NUM_INSERTIONS]), 0, 50);
+            g.drawString(
+                "Avg. Comparisons: " + String.format("%.2f", (double) numComparisons) + " " + 
+                "Avg. Swaps: "       + String.format("%.2f", (double) numSwaps) + " " +  
+                "Avg. Insertions: "  + String.format("%.2f", (double) numInsertions), 0, 50);
 
             // Y position of the restart button
-            int restartYPosition= 57;
-            if (data[Constants.DATA_INDICES.NUM_TIME] > 0) {
-                restartYPosition= 100 - 18;
-                graphics.drawString("Avg. Time (nanoseconds): " + String.format("%,d", data[Constants.DATA_INDICES.NUM_TIME]/(data[Constants.DATA_INDICES.NUM_SIMULATIONS]-1)), 0, 75);
+            int restartY = 57;
+            if (totalTime > 0) {
+                restartY = 100 - 18;
+                g.drawString("Avg. Time (nanoseconds): " + String.format("%,d", totalTime/(numSims-1)), 0, 75);
             }
                 
             // Creates the restart button
-            graphics.drawRect(0, restartYPosition, 225, 25);
-            graphics.drawString("New Simulation", 0, restartYPosition + 18);
-            restart= new Rectangle2D.Double(0, restartYPosition, 225, 25);
+            g.drawRect(0, restartY, 225, 25);
+            g.drawString("New Simulation", 0, restartY + 18);
+            restart= new Rectangle2D.Double(0, restartY, 225, 25);
         }
     }
 
@@ -140,11 +138,25 @@ public class GraphScreen {
             main.start();
     }
 
-    public void updateRender(int[] array, int[] pointers, long[] data, Constants.Mode mode) {
-        this.array= array;
-        this.pointers= pointers;
-        this.data= data;        
-        this.mode= mode;
-        render();
+    public void setArrays(int[] array, int[] pointers, int size, State state) {
+        this.array = array;
+        this.pointers = pointers;
+        this.SIZE = size;
+        this.state = state;
     }
+    // public void setArray(int[] array, int size) {
+    //     this.array = array;
+    //     this.SIZE = size;
+    // }
+    public void setBox(int[] data) {
+        this.numComparisons = data[0];
+        this.numSwaps = data[1];
+        this.numInsertions = data[2];
+    }
+    public void setFinish(int numSims, long totalTime) {
+        finished = true;
+        this.numSims = numSims;
+        this.totalTime = totalTime;
+    }
+
 }
