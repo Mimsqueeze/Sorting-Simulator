70f09578ce0a5a53e304f9827b7075d7246e6068
jmei2657 <panda2jzn@gmail.com>
2023-04-15 11:31:33 -0400

    cleaned up Main.java
    cleaned up Main.java (added comments)

--- src/Main.java
+++ src/Main.java
@@ -19,6 +19,7 @@ public class Main {
         Constants.SORTING_ALG_NAMES.BOZO,
         Constants.SORTING_ALG_NAMES.CUSTOM
     };
+    // All the Java Swing components used for the simulation 
     JFrame frame;
     JPanel panel;
     JComboBox<String> dropDown;
@@ -32,7 +33,6 @@ public class Main {
     JButton exit;
     Font myFont = new Font("Courier New", Font.BOLD, 25);
     Font titleFont = new Font("Courier New", Font.BOLD, 35);
-    GraphScreen graphScreen;
     JLabel title;
     JLabel algorithmText;
     JLabel sizeText;
@@ -41,58 +41,68 @@ public class Main {
     JLabel everyText;
     JLabel soundText;
     JLabel waitText;
+    // GraphScreen object to visualize the sorting algorithm 
+    GraphScreen graphScreen;
+    // Boolean for whether or not the sound is turned on 
     boolean soundOn;
     Main() {
         frame = new JFrame("Sorting Simulator");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         panel = new JPanel();
         panel.setBackground(Color.black);
+        // Starts the simulation at the selection screen 
         start();
     }
     public void start() {
+        // Clears the panel and creates the layout/dimensions of the panel 
         panel.removeAll();
         panel.setLayout(new GridLayout(25, 1));
         panel.setPreferredSize(new Dimension(Constants.SCREEN_SIZES.WIDTH, Constants.SCREEN_SIZES.HEIGHT));
         frame.setContentPane(panel);
 
+        // Title 
         title = new JLabel("Sorting Algorithm Simulator");
         title.setFont(titleFont);
         title.setForeground(Color.green);
         panel.add(title);
 
+        // Divider 
         panel.add(Box.createRigidArea(new Dimension(10, 0)));
 
+        // Dropdown menu for sorting algorithm selection 
         algorithmText = new JLabel("Sorting Algorithm");
         algorithmText.setFont(myFont);
         algorithmText.setForeground(Color.green);
         panel.add(algorithmText);
-
         dropDown = new JComboBox<>(sortingAlgs);
         dropDown.setFont(myFont);
         panel.add(dropDown);
 
+        // Divider
         panel.add(Box.createRigidArea(new Dimension(10, 0)));
 
+        // Section for input of the size of the array to be sorted
         sizeText = new JLabel("Enter Size");
         sizeText.setFont(myFont);
         sizeText.setForeground(Color.green);
         panel.add(sizeText);
-
+        // Adds the text field for the input
         sizeInput.setFont(myFont);
         panel.add(sizeInput);
 
         panel.add(Box.createRigidArea(new Dimension(10, 0)));
 
+        // Section to input the number of simulations
         simText = new JLabel("Enter Number of Simulations");
         simText.setFont(myFont);
         simText.setForeground(Color.green);
         panel.add(simText);
-
         simInput.setFont(myFont);
         panel.add(simInput);
 
         panel.add(Box.createRigidArea(new Dimension(10, 0)));
 
+        // Checkbox for whether the user wants to use their own dataset/array
         customText = new JLabel("Use Custom Dataset");
         customText.setFont(myFont);
         customText.setForeground(Color.green);
@@ -101,6 +111,7 @@ public class Main {
 
         panel.add(Box.createRigidArea(new Dimension(10, 0)));
 
+        // Option to go step by step and click continue to go to the next step
         waitText = new JLabel("Wait For Click To Continue");
         waitText.setFont(myFont);
         waitText.setForeground(Color.green);
@@ -109,6 +120,7 @@ public class Main {
 
         panel.add(Box.createRigidArea(new Dimension(10, 0)));
 
+        // Option to show every simulation (important if the user chooses to run the sorting algorithm more than once)
         everyText = new JLabel("Show Every Simulation");
         everyText.setFont(myFont);
         everyText.setForeground(Color.green);
@@ -117,6 +129,7 @@ public class Main {
 
         panel.add(Box.createRigidArea(new Dimension(10, 0)));
 
+        // Option to add sound
         soundText = new JLabel("Sound");
         soundText.setFont(myFont);
         soundText.setForeground(Color.green);
@@ -124,6 +137,8 @@ public class Main {
         panel.add(soundCheckBox);
 
         panel.add(Box.createRigidArea(new Dimension(10, 0)));
+
+        // Start and Exit button
         run = new JButton("Start Simulation");
         run.setFont(myFont);
         panel.add(run);
@@ -132,8 +147,10 @@ public class Main {
         exit.setFont(myFont);
         panel.add(exit);
 
+        // Adds action listeners to the buttons 
         run.addActionListener(e -> runClick());
         exit.addActionListener(e -> exitClick());
+
         frame.pack();
         frame.setVisible(true);
     }
@@ -141,9 +158,14 @@ public class Main {
         System.exit(0);
     }
     private void runClick() {
+        // Removes all components of the panel (buttons, textfields, etc)
         panel.removeAll();
+
+        // Makes the background black
         panel.getGraphics().setColor(Color.BLACK);
         panel.getGraphics().fillRect(0, 0, Constants.SCREEN_SIZES.WIDTH, Constants.SCREEN_SIZES.HEIGHT);
+        
+        // Adds a mouse listener for the restart and next button
         panel.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
@@ -151,6 +173,8 @@ public class Main {
                 graphScreen.onClick((int) click.getX(), (int) click.getY());
             }
         });
+        
+        // Gets all the information inputted by the user
         int size = Integer.parseInt(sizeInput.getText());
         String algorithm = (String) dropDown.getSelectedItem();
         int numSims = Integer.parseInt(simInput.getText());
